f1cad48f0a2762aaf77d45472dfdc5242a79e195
ayecue <soerenwehmeier@googlemail.com>
2021-07-02 19:47:02 +0200

    0.2.6 use codemirror

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,17 +1,43 @@
 const build = require('../build-light');
 const VM = require('../emulator/ui/vm');
-const Editor = require('./src/components/editor');
 const Stdout = require('./src/utils/Stdout');
 const Stdin = require('./src/utils/Stdin');
+const codemirror = require('@codemirror/basic-setup');
+const EditorView = codemirror.EditorView;
+const EditorState = codemirror.EditorState;
+const basicSetup = codemirror.basicSetup;
+const javascript = require('@codemirror/lang-javascript').javascript;
 
-hljs.initHighlightingOnLoad();
+const transpileEl = document.getElementById('transpile');
+const transpileOutputEl = document.getElementById('toutput');
+const uglifyEl = document.getElementById('uglify');
+const executeEl = document.getElementById('execute');
+const stdoutEl = document.getElementById('stdout');
+const stdinEl = document.getElementById('stdin');
+const editorState = EditorState.create({
+	extensions: [basicSetup, javascript()]
+});
+const editorView = new EditorView({
+	state: editorState,
+	parent: document.getElementById('container')
+});
+const vmInstance = new VM();
+const stdout = new Stdout(stdoutEl);
+const stdin = new Stdin(stdinEl);
 
-ReactDOM.render(
-    React.createElement(Editor, {
-    	build: build,
-    	VM: VM,
-    	Stdout: Stdout,
-    	Stdin: Stdin
-    }),
-    document.getElementById('container')
-);
+vmInstance.start(stdout, stdin);
+
+transpileEl.addEventListener('click', () => {
+	const output = build({
+		uglify: uglifyEl.checked,
+		content: editorView.state.doc.toString()
+	});
+
+	transpileOutputEl.value = output;
+});
+
+executeEl.addEventListener('click', () => {
+	const shell = vmInstance.getLastSession();
+
+	shell.run(editorView.state.doc.toString());
+});
