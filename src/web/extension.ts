aaa7afed8dc024e3361dbde9f0301e470539ae24
ayecue <soerenwehmeier@googlemail.com>
2022-11-22 16:17:56 +0100

    1.6.0 add definition provider in web-ui

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -1,11 +1,13 @@
 import Monaco from 'monaco-editor/esm/vs/editor/editor.api';
 
 import { activate as activateAutocomplete } from './extension/autocomplete';
+import { activate as activateDefinition } from './extension/definition';
 import { activate as activateHover } from './extension/hover';
 
 export function activate(monaco: typeof Monaco) {
   activateHover(monaco);
   activateAutocomplete(monaco);
+  activateDefinition(monaco);
 }
 
 export function deactivate() {}
---
+++ src/web/extension/definition.ts
@@ -0,0 +1,66 @@
+import { ASTBase, ASTIdentifier, ASTMemberExpression } from 'greyscript-core';
+import Monaco from 'monaco-editor/esm/vs/editor/editor.api';
+
+import {
+  LookupHelper
+} from './helper/lookup-type';
+import ASTStringify from './helper/ast-stringify';
+
+const findAllDefinitions = (monaco: typeof Monaco, helper: LookupHelper, identifer: string, root: ASTBase): Monaco.languages.LocationLink[] => {
+  const assignments = helper.findAllAssignmentsOfIdentifier(identifer, root);
+  const definitions: Monaco.languages.LocationLink[] = [];
+
+  for (const assignment of assignments) {
+    if (!assignment.start || !assignment.end) {
+      continue;
+    }
+
+    const definitionLink: Monaco.languages.LocationLink = {
+      uri: helper.document.uri,
+      range: new monaco.Range(
+        assignment.start.line,
+        assignment.start.character - 1,
+        assignment.end.line,
+        assignment.end.character
+      )
+    };
+
+    definitions.push(definitionLink);
+  }
+
+  return definitions;
+}
+
+export function activate(monaco: typeof Monaco) {
+  monaco.languages.registerDefinitionProvider('greyscript', {
+    provideDefinition(
+      document: Monaco.editor.ITextModel,
+      position: Monaco.Position,
+      _token: Monaco.CancellationToken
+    ): Monaco.languages.ProviderResult<Monaco.languages.LocationLink[]> {
+      const helper = new LookupHelper(monaco, document);
+      const astResult = helper.lookupAST(position);
+
+      if (!astResult) {
+        return [];
+      }
+
+      const { outer, closest } = astResult;
+
+      if (!(closest instanceof ASTIdentifier)) {
+        return [];
+      }
+
+      const previous = outer.length > 0 ? outer[outer.length - 1] : undefined;
+      let identifer = closest.name;
+      
+      if (previous && previous instanceof ASTMemberExpression) {
+        identifer = ASTStringify(previous);
+      }
+
+      const definitions = findAllDefinitions(monaco, helper, identifer, closest.scope!);
+
+      return definitions;
+    }
+  });
+}
