5d993681345e9cbc6c2f30cc8ba6b933ba6342bf
ayecue <soerenwehmeier@googlemail.com>
2023-05-19 01:52:23 +0200

    1.8.7 add constants, keywords + operators in autocomplete

---
+++ src/web/extension/autocomplete/operators.ts
@@ -0,0 +1,43 @@
+import { Operator } from 'greyscript-core';
+import Monaco from 'monaco-editor/esm/vs/editor/editor.api.js';
+
+import { PseudoCompletionItem } from '../helper/vs.js';
+
+export const AVAILABLE_OPERATORS = [
+  Operator.Plus,
+  Operator.Asterik,
+  Operator.Minus,
+  Operator.Slash,
+  Operator.Power,
+  Operator.PercentSign,
+  Operator.LessThan,
+  Operator.GreaterThan,
+  Operator.LessThanOrEqual,
+  Operator.GreaterThanOrEqual,
+  Operator.NotEqual,
+  Operator.Equal,
+  Operator.AddShorthand,
+  Operator.SubtractShorthand,
+  Operator.MultiplyShorthand,
+  Operator.DivideShorthand,
+  Operator.BitwiseAnd,
+  Operator.BitwiseOr,
+  Operator.LeftShift,
+  Operator.RightShift,
+  Operator.UnsignedRightShift,
+  Operator.Assign,
+  Operator.Reference
+] as const;
+
+export const getAvailableOperators = (
+  range: Monaco.Range
+): PseudoCompletionItem[] => {
+  return AVAILABLE_OPERATORS.map((label: string) => {
+    return new PseudoCompletionItem({
+      label,
+      kind: Monaco.languages.CompletionItemKind.Operator,
+      insertText: label,
+      range
+    });
+  });
+};
