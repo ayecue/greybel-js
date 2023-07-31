f1092503e9ec2779bc4a75c7658c392b04ad6602
ayecue <soerenwehmeier@googlemail.com>
2023-07-31 21:37:42 +0200

    2.0.5 update packages

--- src/web/execute/output.ts
+++ src/web/execute/output.ts
@@ -4,14 +4,14 @@ import {
   OutputHandler,
   PrintOptions
 } from 'greybel-interpreter';
-import { Tag, TagRecord, transform } from 'text-mesh-transformer';
+import { Tag, TagRecordOpen, transform } from 'text-mesh-transformer';
 
 import { Stdin, Stdout } from '../std.js';
 
-function wrapWithTag(openTag: TagRecord, content: string): string {
-  switch (openTag.tag) {
+function wrapWithTag(openTag: TagRecordOpen, content: string): string {
+  switch (openTag.type) {
     case Tag.Color:
-      return `<span style="color:${openTag.value};">${content}</span>`;
+      return `<span style="color:${openTag.attributes.value};">${content}</span>`;
     case Tag.Underline:
       return `<span style="text-decoration:underline;">${content}</span>`;
     case Tag.Italic:
@@ -21,36 +21,36 @@ function wrapWithTag(openTag: TagRecord, content: string): string {
     case Tag.Strikethrough:
       return `<span style="text-decoration:line-through;">${content}</span>`;
     case Tag.Mark:
-      return `<span style="background-color:${openTag.value};">${content}</span>`;
+      return `<span style="background-color:${openTag.attributes.value};">${content}</span>`;
     case Tag.Lowercase:
       return `<span style="text-transform:lowercase;">${content}</span>`;
     case Tag.Uppercase:
       return `<span style="text-transform:uppercase;">${content}</span>`;
     case Tag.Align:
-      return `<span style="text-align:${openTag.value};display:block;">${content}</span>`;
+      return `<span style="text-align:${openTag.attributes.value};display:block;">${content}</span>`;
     case Tag.CSpace:
-      return `<span style="letter-spacing:${openTag.value};">${content}</span>`;
+      return `<span style="letter-spacing:${openTag.attributes.value};">${content}</span>`;
     case Tag.LineHeight:
-      return `<span style="line-height:${openTag.value};">${content}</span>`;
+      return `<span style="line-height:${openTag.attributes.value};">${content}</span>`;
     case Tag.Margin:
-      return `<span style="margin:0 ${openTag.value};">${content}</span>`;
+      return `<span style="margin:0 ${openTag.attributes.value};">${content}</span>`;
     case Tag.NoBR:
       return `<nobr>${content}</nobr>`;
     case Tag.Pos:
-      return `<span style="position:absolute;top:${openTag.value};">${content}</span>`;
+      return `<span style="position:absolute;top:${openTag.attributes.value};">${content}</span>`;
     case Tag.Size:
-      return `<span style="font-size:${openTag.value};">${content}</span>`;
+      return `<span style="font-size:${openTag.attributes.value};">${content}</span>`;
     case Tag.VOffset:
-      return `<span style="margin-top:${openTag.value};">${content}</span>`;
+      return `<span style="margin-top:${openTag.attributes.value};">${content}</span>`;
     case Tag.Indent:
-      return `<span style="margin-left:${openTag.value};">${content}</span>`;
+      return `<span style="margin-left:${openTag.attributes.value};">${content}</span>`;
   }
 
-  if (openTag.value) {
-    return `&lt${openTag.tag}&#61;${openTag.value}&gt;${content}&lt/${openTag.tag}&gt;`;
+  if (openTag.attributes.value) {
+    return `&lt${openTag.type}&#61;${openTag.attributes.value}&gt;${content}&lt/${openTag.type}&gt;`;
   }
 
-  return `&lt${openTag.tag}&gt;${content}&lt/${openTag.tag}&gt;`;
+  return `&lt${openTag.type}&gt;${content}&lt/${openTag.type}&gt;`;
 }
 
 export class WebOutputHandler extends OutputHandler {
@@ -70,7 +70,7 @@ export class WebOutputHandler extends OutputHandler {
   ) {
     const transformed = transform(
       message,
-      (openTag: TagRecord, content: string): string => {
+      (openTag: TagRecordOpen, content: string): string => {
         return wrapWithTag(openTag, content);
       }
     ).replace(/\\n/g, '\n');
