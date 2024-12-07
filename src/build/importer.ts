f1b6f7ef45cc34aa6b33486b2510179412521c7e
ayecue <soerenwehmeier@googlemail.com>
2024-12-07 17:58:29 +0100

    3.5.14 improve import error report

--- src/build/importer.ts
+++ src/build/importer.ts
@@ -217,9 +217,75 @@ export const parseImporterMode = (mode: string): ImporterMode => {
   }
 };
 
-export const createImporter = async (
+enum CommonImportErrorReason {
+  NoAvailableSocket = 'There is no available socket!',
+  NewGameVersion = 'A new game update is available.'
+}
+
+const reportFailure = (failedItems: ImportResultFailure[], agentType: AgentType): void => {
+  const uniqueErrorReasons = new Set(failedItems.map((it) => it.reason));
+
+  if (uniqueErrorReasons.size === 1) {
+    const singularErrorReason = failedItems[0].reason;
+
+    if (singularErrorReason.indexOf(CommonImportErrorReason.NoAvailableSocket) !== -1) {
+      if (agentType === AgentType.C2Light) {
+        logger.debug(`File import failed!
+          
+The issue appears to be due to the lack of an available socket. This could suggest that the BepInEx plugin is not installed correctly, or the game is not running. Double-check the plugin installation and ensure the game is running.
+  
+For detailed troubleshooting steps, please consult the documentation: https://github.com/ayecue/greybel-vs?tab=readme-ov-file#message-hook.`
+        );
+        return;
+      }
+
+      logger.debug(`File import failed!
+        
+The issue appears to be due to the lack of an available socket. This might indicate that the game is not running. Please ensure the game is started before proceeding. Additionally note that it is recommend to rather use the message-hook agent but that requires installing BepInEx: https://github.com/ayecue/greybel-vs?tab=readme-ov-file#message-hook.
+
+For detailed troubleshooting steps related to the headless agent, please consult the documentation: https://github.com/ayecue/greybel-vs?tab=readme-ov-file#headless.`
+      );
+      return;
+    } else if (singularErrorReason.indexOf(CommonImportErrorReason.NewGameVersion) !== -1) {
+      logger.debug(`File import failed!
+      
+It seems that the game has received an update. This can sometimes cause issues with the import process. Please wait for the Greybel developers to update the game and try again later.`
+      );
+      return;
+    }
+    logger.debug(`File import failed!
+  
+The reason seems to be unknown for now. Please either join the discord or create an issue on GitHub.`
+    );
+
+
+    return;
+  }
+
+  logger.debug(`File import failed!
+    
+The reason seems to be unknown for now. Please either join the discord or create an issue on GitHub.`
+  );
+}
+
+export const executeImport = async (
   options: ImporterOptions
-): Promise<ImportResult[]> => {
-  const installer = new Importer(options);
-  return await installer.import();
+): Promise<boolean> => {
+  const importer = new Importer(options);
+  const results = await importer.import();
+
+  const successfulItems = results.filter((item) => item.success) as ImportResultSuccess[];
+  const failedItems = results.filter((item) => !item.success) as ImportResultFailure[];
+
+  if (successfulItems.length === 0) {
+    reportFailure(failedItems, options.agentType);
+    return false;
+  } else if (failedItems.length > 0) {
+    logger.debug(`Import was only partially successful. Only ${successfulItems.length} files got imported to ${options.ingameDirectory}!`);
+    return false;
+  }
+
+  logger.debug(`${successfulItems.length} files got imported to ${options.ingameDirectory}!`);
+
+  return true;
 };
